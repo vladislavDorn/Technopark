0ca66a95a63a35fd82e2c397f9b36731864ce0bf
vladislavDorn <olifirenkovlad@gmail.com>
2019-03-10 12:47:03 +0600

    add map and fixed adaptive layout

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//Header navbar btn
 const menuOne = document.querySelector('.menuOne');
 function addClassFunOne() {
     this.classList.toggle("clickMenuOne");
@@ -48,7 +49,6 @@ $('.partners__owlcarousel').owlCarousel({
     }
 });
 
-
 //slider gallery
 $('.gallery__owlcarousel').owlCarousel({
     loop:true,
@@ -68,3 +68,72 @@ $('.gallery__owlcarousel').owlCarousel({
       }
   }
 });
+
+//contact map
+// // Функция ymaps.ready() будет вызвана, когда
+//     // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+//     ymaps.ready(init);
+//     function init(){ 
+//         // Создание карты.    
+//         var myMap = new ymaps.Map("map", {
+//             // Координаты центра карты.
+//             // Порядок по умолчанию: «широта, долгота».
+//             // Чтобы не определять координаты центра карты вручную,
+//             // воспользуйтесь инструментом Определение координат.
+//             center: [42.85698638164069,74.63462535160292],
+//             // Уровень масштабирования. Допустимые значения:
+//             // от 0 (весь мир) до 19.
+//             zoom: 17
+//         });
+//     }
+    ymaps.ready(function () {
+      var myMap = new ymaps.Map('map', {
+              center: [42.85724148476384,74.63450191169608],
+              zoom: 17
+          }, {
+              searchControlProvider: 'yandex#search'
+          }),
+  
+          // Создаём макет содержимого.
+          MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+              '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+          ),
+  
+          myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+              hintContent: 'Собственный значок метки',
+              balloonContent: 'Это красивая метка'
+          }, {
+              // Опции.
+              // Необходимо указать данный тип макета.
+              iconLayout: 'default#image',
+              // Своё изображение иконки метки.
+              iconImageHref: 'image/anchor.png',
+              // Размеры метки.
+              iconImageSize: [30, 42],
+              // Смещение левого верхнего угла иконки относительно
+              // её "ножки" (точки привязки).
+              iconImageOffset: [-5, -38]
+          }),
+  
+          myPlacemarkWithContent = new ymaps.Placemark([42.85724148476384,74.63450191169608], {
+          }, {
+              // Опции.
+              // Необходимо указать данный тип макета.
+              iconLayout: 'default#imageWithContent',
+              // Своё изображение иконки метки.
+              iconImageHref: 'images/ball.png',
+              // Размеры метки.
+              iconImageSize: [48, 48],
+              // Смещение левого верхнего угла иконки относительно
+              // её "ножки" (точки привязки).
+              iconImageOffset: [-24, -24],
+              // Смещение слоя с содержимым относительно слоя с картинкой.
+              iconContentOffset: [15, 15],
+              // Макет содержимого.
+              iconContentLayout: MyIconContentLayout
+          });
+  
+      myMap.geoObjects
+          .add(myPlacemark)
+          .add(myPlacemarkWithContent);
+  });
